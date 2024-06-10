e096610c00f3e0b28b8833647b3c499340ce2f5e
saito haruto <haruto3110.2001@gmail.com>
2024-06-10 17:41:43 +0900

    feat: 問い合わせ機能の作成中

---
+++ app/api/email/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse, type NextRequest } from "next/server";
+import nodemailer from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
+import { env } from "process";
+import { text } from "stream/consumers";
+
+export async function POST(request: NextRequest) {
+    const {email, message }  =  await request.json();
+
+    const transport = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.NODEMAILER_EMAIL,
+            pass: process.env.NODEMAILER_PASSWORD,
+        },
+    })
+
+    const mailOptions: Mail.mailOptions = {
+        from: env.NODEMAILER_EMAIL,
+        to: env.NODEMAIRER_EMAIL,
+        subject: `Message from ${email}`,
+        text: message,
+    }
+
+    try {
+        await transport.sendMail(mailOptions);
+        return NextResponse.json({ message: "Sucess!", status: 200 })
+    } catch (err) {
+        return NextResponse.json({ message: "Failed! ", status: 500 })
+    }
+}
---
+++ app/components/ContactForm.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useState } from "react";
+import Button from '@mui/material/Button';
+
+function ContactForm() {
+    const[ email, setEmail ] = useState('');
+    const[ message, setMessage ] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        await fetch("/api/email", {
+            method: "POST",
+            body: JSON.stringify({ email, message }),
+        })
+    }
+
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <form onSubmit={handleSubmit} className="space-y-10">
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Message:</label>
+                    <textarea
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        required
+                    />
+                </div>
+                <Button type="submit">Submit</Button>
+            </form>
+        </div>
+    )
+
+}
+export default ContactForm;
